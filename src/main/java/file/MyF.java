638e214f04b78d708319d3491aa700b8f75f6368
sehrivaliyeva <114057052+sehrivaliyeva@users.noreply.github.com>
2024-09-12 13:29:12 +0400

    .

--- src/main/java/file/MyF.java
+++ src/main/java/file/MyF.java
@@ -1,10 +1,15 @@
 package file;
 
-import java.io.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class MyF {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         File file = new File("C:\\Users\\28 Programmers\\Desktop\\MyFile.txt");
         /*Writer wr = null;
         try {
@@ -25,21 +30,70 @@ public class MyF {
         }*/
 
 
-        Reader reader = null;
+      /* Reader reader = null;
         try {
-            reader=new FileReader(file);
-            char [] array1;
-            array1=new char[reader.read()];
-           // while(array1.)
-           // System.out.println(metn);
+            reader = new FileReader(file);
+            char[] array1 = new char[(int) file.length()];
+            reader.read(array1);
+            System.out.println(array1);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             reader.close();
-        }
+        }*/
+
+        //Byte tipli melumati yazmaq
+
+      /*  OutputStream outputStream = new FileOutputStream(file);
+        String soz = "salam men byte yaziram ";
+        byte[] array = soz.getBytes();
+        outputStream.write(array);
+        outputStream.close();*/
+
+        //byte tipli melumati oxumaq
+
+      /*  InputStream inputStream = new FileInputStream(file);
+        byte[] array = new byte[(int) file.length()];
+        inputStream.read(array);
+        inputStream.close();
+        String s=new String(array);
+        System.out.println(s);*/
+
+
+        //Object tipli melumat yazmaq
+
+
+       /* ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file));
+        Melumat melumat = new Melumat("Gunel", "Mammadova");
+        Melumat melumat2 = new Melumat("Arzu", "Mammadova");
+        outputStream.writeObject(melumat);
+        outputStream.close();*/
+
+        //Object tipli melumat oxumaq
+
+
+        /*ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+        Melumat melumat = (Melumat) objectInputStream.readObject();
+        System.out.println(melumat.getName() + " " + melumat.getSurname());*/
+
+
+        //Path haqqinda nio-dan gelir interfacedir Paths vasitesile yaradilir new ile olmur
+        //Files final classdir path metodlarina komek edir
+
+        Path path = Paths.get(file.toURI());
+        Path path2 = Paths.get(file.toURI());
+
+        //Files classi
+        boolean isSameFile = Files.isSameFile(path, path2);
+        long a = Files.size(path);
+
+
+
+        //File ozu bir classdir metodlari var dahah kohne classdir
+
 
     }
 }
