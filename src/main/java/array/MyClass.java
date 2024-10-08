f047a02a361a49c86252b32ce7c48fd32b1a22ac
sehrivaliyeva <sehrigul.valiyeva02@gmail.com>
2024-10-08 10:49:41 +0300

    .

---
+++ src/main/java/array/MyClass.java
@@ -0,0 +1,13 @@
+package array;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Integer arr[]={1,5,6};
+        //int olanda [I cixir console
+        //Integer olanda ise [Ljava.lang.Integer;
+        System.out.println(arr.getClass().getName());
+       // System.arraycopy();
+       // arr.clone();
+
+    }
+}
